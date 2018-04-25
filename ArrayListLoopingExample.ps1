6cc4bd93718dc7f94b844c7c5da7af6a60da3166
simplescripter <simplescripter@gmail.com>
2018-04-25 15:19:32 -0600

    Added ArrayListLoopingExample.ps1

---
+++ ArrayListLoopingExample.ps1
@@ -0,0 +1,14 @@
+﻿# An example of looping through an arraylist of computer names and removing
+# those names that responded.  Loops until all systems have been contacted.
+
+[System.Collections.ArrayList]$computerList = 'LON-DC1','LON-SVR1','LON-CL1'
+Do {
+    [array]$list = $computerList
+    ForEach($computer in $list){
+        If(Test-Connection -ComputerName $computer -Count 1 -Quiet){
+            $computerList.Remove($computer)
+        }
+    }
+    If($computerList.Count -gt 0){Sleep 5}
+}Until($computerList.Count -eq 0)
+Write-Host 'All systems in $computerList have been contacted.' -ForegroundColor DarkCyan -BackgroundColor Yellow
