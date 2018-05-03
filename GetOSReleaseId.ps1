3c0fd5f6460473c406d63b0b8f526000ca5d1289
simplescripter <simplescripter@gmail.com>
2018-05-03 11:53:36 -0400

    Created GetOSReleaseID

---
+++ GetOSReleaseId.ps1
@@ -0,0 +1,25 @@
+﻿Function Get-OSReleaseID {
+    [CmdletBinding()]
+    Param (
+        [Parameter(ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        [string[]]$computerName = 'localhost'
+    )
+    Process {
+        $HKLM = 2147483650
+        $key = "SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+        $releaseID = "ReleaseId"
+        $productName = "ProductName"
+        ForEach($computer in $computerName){
+            $wmi = [wmiclass]"\\$computer\root\default:stdRegProv"
+            $releaseIDValue = ($wmi.GetStringValue($HKLM,$key,$releaseID)).sValue
+            $productNameValue = ($wmi.GetStringValue($HKLM,$key,$productName)).sValue
+            $properties = @{
+                ComputerName = $computerName
+                ProductName = $productNameValue
+                ReleaseID = $releaseIDValue
+            }
+            $return = New-Object -TypeName PSObject -Property $properties
+            Write-Output $return
+        }
+    }
+}
