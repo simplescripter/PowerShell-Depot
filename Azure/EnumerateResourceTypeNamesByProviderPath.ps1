6a36bac31849476d4216f80357a4b235cea0e56c
simplescripter <simplescripter@gmail.com>
2018-06-01 15:21:45 -0600

    New script

---
+++ Azure/EnumerateResourceTypeNamesByProviderPath.ps1
@@ -0,0 +1,12 @@
+﻿$results = @()
+Get-AzureRmResourceProvider | ForEach-Object {
+    $resources = $_ | Select -ExpandProperty ResourceTypes | Select ResourceTypeName
+    ForEach($resource in $resources){
+        $properties = @{
+            ResourceTypeName = "$($resource.ResourceTypeName)"
+            ProviderPath = "$($_.ProviderNameSpace)/$($resource.ResourceTypeName)"
+        }
+        $results += New-Object -TypeName PSObject -Property $properties
+    }
+}
+$results | Sort ResourceTypeName
