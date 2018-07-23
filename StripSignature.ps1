5a2d52f9528c216fb98d0865d9fa9712dfab6164
simplescripter <simplescripter@gmail.com>
2018-07-23 12:12:19 -0600

    Updated formating to StripSignature

--- StripSignature.ps1
+++ StripSignature.ps1
@@ -2,27 +2,20 @@
 # a signed script.
 # Example 1: dir file.ps1 | Strip-Sig
 # Example 2: dir -filter *.ps1 | Strip-Sig
-Function global:Strip-Sig()
-{
- $sig = "# SIG # Begin signature block"
- ForEach ($_ in $input){
- $j = $null
- Write-Host -ForegroundColor Green "Stripping $_"
- $filestream = cat $_
- Clear-Content $_
- ForEach($i in $filestream)
- {
-  if($i -ne $sig)
-  {
-    $j = $j + $i + "`n";
-  }
-  Else
-  {
-   Break
-  } 
- }
- Set-Content $_ $j
- }
+Function global:Strip-Signature {
+     $sig = "# SIG # Begin signature block"
+     ForEach ($_ in $input){
+         $j = $null
+         Write-Host -ForegroundColor Green "Stripping $_"
+         $filestream = cat $_
+         Clear-Content $_
+         ForEach($i in $filestream){
+              If($i -ne $sig){
+                $j = $j + $i + "`n"
+              }Else{Break} 
+         }
+         Set-Content $_ $j
+     }
 }
 
 
