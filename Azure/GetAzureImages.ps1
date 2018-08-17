fbb6f5384a648e6fc8580873860eee150c910302
simplescripter <simplescripter@gmail.com>
2018-08-17 11:23:46 -0600

    Updates
    Check for Azure log on
    Output image result(s) to Out-Gridview then to pipeline

--- Azure/GetAzureImages.ps1
+++ Azure/GetAzureImages.ps1
@@ -1,7 +1,11 @@
-﻿Add-AzureRmAccount
+﻿Try{
+    Get-AzureRmContext -ErrorAction Stop 
+}Catch{
+    Add-AzureRmAccount
+}
 $location = Get-AzureRmLocation | Out-GridView -PassThru -Title "SELECT A LOCATION AND CLICK OK" | Select -ExpandProperty Location
 $publisher = Get-AzureRmVMImagePublisher -Location $location | Out-GridView -PassThru -Title "SELECT A PUBLISHER AND CLICK OK" | Select -ExpandProperty PublisherName
 $offer = Get-AzureRmVMImageOffer -Location $location -PublisherName $publisher | Out-GridView -PassThru -Title "SELECT AN OFFER AND CLICK OK" | Select -ExpandProperty Offer 
 $sku = Get-AzureRmVMImageSKU -Location $location -PublisherName $publisher -Offer $offer |
     Out-GridView -PassThru -Title "SELECT AN SKU AND CLICK OK" | Select -ExpandProperty SKUs
-Get-AzureRmVMImage -Location $location -PublisherName $publisher -Offer $offer -Skus $sku
+Get-AzureRmVMImage -Location $location -PublisherName $publisher -Offer $offer -Skus $sku | Out-Gridview -PassThru -Title "SELECT AN IMAGE and CLICK OK"
