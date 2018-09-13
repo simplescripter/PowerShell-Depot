7498fa38cba73b5041410641761bb52d9d047f2b
simplescripter <simplescripter@gmail.com>
2018-09-13 15:47:55 -0600

    Created RegisterRepo.ps1

---
+++ RegisterRepo.ps1
@@ -0,0 +1,16 @@
+﻿Function Register-Repo {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$name,
+
+        [Parameter(Mandatory=$true)]
+        [string]$sourceLocation,
+
+        [ValidateSet('Trusted','Untrusted')]
+        [string]$installationPolicy = 'Trusted'
+    )
+    # Many sites, including GitHub, now require TLS 1.2 or greater, so let's enable it:
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    Register-PSRepository -Name $name -SourceLocation $sourceLocation -InstallationPolicy $installationPolicy
+}
