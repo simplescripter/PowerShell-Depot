c74055dd0389f95c5df4075bf70d69def2665ef7
Shawn Stugart <simplescripter@gmail.com>
2018-10-04 14:45:05 -0600

    Create CopyDirectoryToSystems.ps1

---
+++ CopyDirectoryToSystems.ps1
@@ -0,0 +1,7 @@
+# An example for copying a source folder to one or more destination systems
+
+$folderPath = 'E:\drivers'
+$computers = Import-CSV E:\computers.csv
+ForEach($computer in $computers){
+
+}
