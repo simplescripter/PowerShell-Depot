cfdea40d39e64a9909149999e606caa41e4d9521
Shawn Stugart <simplescripter@gmail.com>
2018-10-04 14:47:11 -0600

    Update CopyDirectoryToSystems.ps1

--- CopyDirectoryToSystems.ps1
+++ CopyDirectoryToSystems.ps1
@@ -3,5 +3,7 @@
 $folderPath = 'E:\drivers'
 $computers = Import-CSV E:\computers.csv
 ForEach($computer in $computers){
-
+    New-PSDrive -Name Z -Root ("\\" + $computer.name + '\C$') -PSProvider FileSystem
+    Copy-Item -Path $folderPath -Recurse -Force -Destination Z:\
+    Remove-PSDrive -Name Z -Force
 }
