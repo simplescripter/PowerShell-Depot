98e9afdb77f86fda5c33447f7639d56dd9c31021
simplescripter <simplescripter@gmail.com>
2018-10-05 14:13:01 -0600

    updates

--- SkylakeCheck.ps1
+++ SkylakeCheck.ps1
@@ -1,7 +1,8 @@
-﻿$systems = Get-Content E:\systems.txt
+﻿$systems = Get-Content C:\script\systems.txt
 $skylakeList = Invoke-WebRequest 'http://ark.intel.com/products/codename/37572/Skylake#@All'
 ForEach ($system in $systems){
-    $processor = ((Get-WMIObject win32_Processor -ComputerName $system).name) -replace '.*(\w{0,1}\d{4}\w{0,1}).*','$1'
+    $processor = ((Get-WMIObject win32_Processor -ComputerName $system).name) `
+        -replace '.*(\w{0,1}\d{4}\w{0,1}).*','$1'
     $properties = @{
         ComputerName = $system
         Skylake = $skylakeList -match $processor
