b6ce24131d3873aa1fddad8173fe6b24ca08f6a6
simplescripter <simplescripter@gmail.com>
2018-10-25 06:42:40 -0600

    Added ConsoleColorCombos.ps1

---
+++ ConsoleColorCombos.ps1
@@ -0,0 +1,26 @@
+﻿# Possible console text and background colors:
+$colors = 'Black',
+    'Blue',
+    'Cyan',
+    'DarkBlue',
+    'DarkCyan',
+    'DarkGray',
+    'DarkGreen',
+    'DarkMagenta',
+    'DarkRed',
+    'DarkYellow',
+    'Gray',
+    'Green',
+    'Magenta',
+    'Red',
+    'White',
+    'Yellow'
+
+    ForEach ($foregroundColor in $colors){
+        ForEach ($backgroundColor in $colors){
+            Write-Host "$foregroundColor on $backgroundColor" -ForegroundColor $foregroundColor -BackgroundColor $backgroundColor
+        }
+    }
+
+
+
