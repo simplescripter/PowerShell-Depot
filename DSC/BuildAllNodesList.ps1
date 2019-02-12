a4fa5a0f5d8de6ddef3c59f4fd45bff9719ec53a
Shawn Stugart <simplescripter@gmail.com>
2019-02-11 20:17:20 -0700

    Add files via upload

---
+++ DSC/BuildAllNodesList.ps1
@@ -0,0 +1,21 @@
+﻿Function Build-AllNodesList {
+    #requires -module  ActiveDirectory
+    $computerList = Get-ADComputer -Filter *
+    $nodes = @()
+    ForEach ($computer in $computerList){
+        $nodes += @"
+          @{
+                NodeName = "$($computer.Name)"
+            }`n
+"@
+    }
+    $AllNodeString = @"
+    `$ConfigurationData =
+        @{
+            AllNodes = @(
+                $nodes
+            )
+        }
+"@
+    Write-Output $AllNodeString
+}
---
+++ DSC/BuildFeatureList.ps1
@@ -0,0 +1,16 @@
+﻿# We can pull the existing feature list from a server and add it to a ConfigurationData hash table.
+#To speed up the process, do this: 
+
+$list = Get-WindowsFeature | Where-Object Installed -eq $true | Select-Object -ExpandProperty Name 
+$list = $list -join ",`n"
+
+@"
+ `$configData = @{
+    AllNodes = @(
+        NodeName = "*"
+        WindowsFeature = @(
+            $($list)
+        )
+    )
+ }
+"@
