8737efde24f8da78879bc4745d9f245cd18164a5
Shawn Stugart <simplescripter@gmail.com>
2019-02-12 14:21:16 -0800

    Replaced xNew-SelfSignedDscCertificate with certutil

--- DSC/DscCertDemo.ps1
+++ DSC/DscCertDemo.ps1
@@ -1,22 +1,55 @@
-﻿# For a DSC Demo using 55202 Lab on Demand VMs
+# For a DSC Demo using 55202 Lab on Demand VMs
 # Run from StudentServer
 
 Param(
-    [string]$computerName = 'StudentServer',
+    [string]$computerName = 'StudentServer2',
     [string]$dirPath = 'C:\DSC'
 )
 
 If(-not (Test-Path $dirPath)){
     New-Item "C:\DSC" -ItemType Directory
 }
+<#  New-xSelfSignedDscEncryptionCertificate doesn't work with older versions of New-SelfSignedCertificate
 
 # Grab xDscUtils if necessary
 If(-not (Get-Module xDscUtils -ListAvailable)){
-    Install-Module xDscUtils -Force
+    Install-Module xDscUtils
 }
 
 # Use New-xSelfSignedDscEncryptionCertificate to generate a self-signed cert for the local host
 $cert = New-xSelfSignedDscEncryptionCertificate -EmailAddress DscCertDemo -ValidityYears 5 -ExportFilePath C:\$computerName.cer
+#>
+
+# Instead of New-xSelfSignedDscEncryptionCertificate, use Microsoft's technique documented with 
+# Protect-CmsMessage:
+
+    # Create .INF file for certreq
+
+{[Version]
+Signature = "$Windows NT$"
+
+[Strings]
+szOID_ENHANCED_KEY_USAGE = "2.5.29.37"
+szOID_DOCUMENT_ENCRYPTION = "1.3.6.1.4.1.311.80.1"
+
+[NewRequest]
+Subject = "cn=DscCertDemo"
+MachineKeySet = false
+KeyLength = 2048
+KeySpec = AT_KEYEXCHANGE
+HashAlgorithm = Sha1
+Exportable = true
+RequestType = Cert
+KeyUsage = "CERT_KEY_ENCIPHERMENT_KEY_USAGE | CERT_DATA_ENCIPHERMENT_KEY_USAGE"
+ValidityPeriod = "Years"
+ValidityPeriodUnits = "1000"
+
+[Extensions]
+%szOID_ENHANCED_KEY_USAGE% = "{text}%szOID_DOCUMENT_ENCRYPTION%"
+} | Out-File -FilePath "$($dirPath)\$($computerName)_DocumentEncryption.inf"
+
+# After you have created your certificate file, run the following command to add the certificate file to the certificate store.Now you are ready to encrypt and decrypt content with the next two examples.
+certreq -new "$($dirPath)\$($computerName)_DocumentEncryption.inf" "$($dirPath)\$($computerName).cer"
 
 # Configuration Data section
 $configData = @{
@@ -26,7 +59,7 @@ $configData = @{
         }
         @{
             NodeName = $computerName
-            CertificateFile = "C:\$computerName.cer" # local path where MOF is being compiled
+            CertificateFile = "C:\DSC\$computerName.cer" # local path where MOF is being compiled
             CertificateID = $cert.Thumbprint
         }
     )
@@ -60,7 +93,7 @@ Configuration LocalAccounts {
 
         Group GroupResourceExample {
           GroupName = "ExampleGroup"
-          Members = "Nicole", "Rocky", "Mark","BlueBuffalo\Administrator"
+          Members = "Bob","BlueBuffalo\Administrator"
           Credential = (Get-Credential -Message 'Enter domain user credentials')
           #DependsOn = "[User]UserResourceExample"
         }
