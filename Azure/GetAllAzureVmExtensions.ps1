3d7a192fb9ff4fa92545632a19ee0f137f3da486
simplescripter <simplescripter@gmail.com>
2019-02-18 21:29:36 -0700

    update

--- Azure/GetAllAzureVmExtensions.ps1
+++ Azure/GetAllAzureVmExtensions.ps1
@@ -3,8 +3,22 @@ Param(
     [string]$location
 )
 
-ForEach($publisher in (Get-AzVmImagePublisher -Location $location | Select-Object -ExpandProperty PublisherName)){
-    ForEach($type in (Get-AzVmExtensionImageType -Location $location -PublisherName $publisher | Select-Object -ExpandProperty Type)){
+# Check for Azure login:
+
+Try{
+    Get-AzContext -ErrorAction Stop | Out-Null
+}Catch{
+    Add-AzAccount
+}
+
+$publishers = Get-AzVmImagePublisher -Location $location | Select-Object -ExpandProperty PublisherName
+$publisherCount = $publishers.Count
+$count = 0
+ForEach($publisher in $publishers){
+    $count++
+    Write-Progress -Activity 'Enumerating Publisher:' -Status $publisher -PercentComplete ($count/$publisherCount * 100)
+    $types = Get-AzVmExtensionImageType -Location $location -PublisherName $publisher | Select-Object -ExpandProperty Type
+    ForEach($type in $types){
         If(Get-AzVMExtensionImage -Location $location -PublisherName $publisher -Type $type -ErrorAction SilentlyContinue){
             $properties = @{
                 'Publisher' = $publisher
