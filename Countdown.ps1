2351216ce74c8af29655aa92b517046932f4a41b
simplescripter <simplescripter@gmail.com>
2019-05-13 06:48:12 -0600

    Changes

---
+++ Countdown.ps1
@@ -0,0 +1,7 @@
+﻿Param (
+    $startingPage = 1
+)
+For($i = $startingPage; $i -le 607;$i++){
+    Write-Host "Page $i, pages remaining = $(607 - $i), percent remaining = $(100 - [math]::Round((($i / 607)*100),2))" -NoNewline -ForegroundColor DarkGray 
+    Read-Host   
+}
