d21704529c9df0d8bedac0c5a28eb7ff84b75020
simplescripter <simplescripter@gmail.com>
2019-05-13 06:51:55 -0600

    Update WeatherDemo.ps1

--- WeatherDemo.ps1
+++ WeatherDemo.ps1
@@ -6,6 +6,21 @@
 #  TempKelvin * 9/5 - 459.67
 $littletonCityID = 5429032
 $apiKey = 'fe5c2a60e7255b20b113c52187629653'
+# By city name:
 #$uri = "api.openweathermap.org/data/2.5/weather?q=Denver,us&mode=json&APPID=$apiKey"
+
+# By city ID:
 $uri = "api.openweathermap.org/data/2.5/weather?id=$littletonCityID&mode=json&APPID=$apiKey"
+
+# Forecast by city ID:
+$forecastURI = "api.openweathermap.org/data/2.5/forecast?id=$littletonCityID&mode=json&APPID=$apiKey"
 $results = Invoke-RestMethod $uri
+       # or
+#$results = Invoke-WebRequest $forecastURI
+
+Function ConvertFrom-Kelvin {
+    Param(
+        [double]$temp
+    )
+    ($temp -273.15) * 9/5 + 32 
+}
