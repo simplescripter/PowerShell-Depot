b60a44cd2c2cb42f455d8bdd1b43202674e09be3
simplescripter <simplescripter@gmail.com>
2019-05-31 12:08:59 -0600

    Create GetOctoCatWisdom.ps1

---
+++ GetOctoCatWisdom.ps1
@@ -0,0 +1,9 @@
+﻿Function Get-OctoCatWisdom {
+    # ReST Demo
+
+    # Many sites, including GitHub, now require TLS 1.2 or greater, so let's enable it:
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    $url = 'https://api.github.com/octocat'
+    Invoke-RestMethod $url
+}
