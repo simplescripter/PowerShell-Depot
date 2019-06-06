392dbb90955278d7dce0888d5a3d70ab6c7e4eeb
simplescripter <simplescripter@gmail.com>
2019-06-06 14:21:37 -0600

    Update GetAzureImages.ps1
    Replaced -Passthru with -OutputMode Single

--- Azure/GetAzureImages.ps1
+++ Azure/GetAzureImages.ps1
@@ -14,9 +14,9 @@ Try{
 
 # Build an Out-Gridview pipeline for selection of an Azure Marketplace VM image:
 
-$location = Get-AzureRmLocation | Out-GridView -PassThru -Title "SELECT A LOCATION AND CLICK OK" | Select -ExpandProperty Location
-$publisher = Get-AzureRmVMImagePublisher -Location $location | Out-GridView -PassThru -Title "SELECT A PUBLISHER AND CLICK OK" | Select -ExpandProperty PublisherName
-$offer = Get-AzureRmVMImageOffer -Location $location -PublisherName $publisher | Out-GridView -PassThru -Title "SELECT AN OFFER AND CLICK OK" | Select -ExpandProperty Offer 
+$location = Get-AzureRmLocation | Out-GridView -OutputMode Single -Title "SELECT A LOCATION AND CLICK OK" | Select -ExpandProperty Location
+$publisher = Get-AzureRmVMImagePublisher -Location $location | Out-GridView -OutputMode Single -Title "SELECT A PUBLISHER AND CLICK OK" | Select -ExpandProperty PublisherName
+$offer = Get-AzureRmVMImageOffer -Location $location -PublisherName $publisher | Out-GridView -OutputMode Single -Title "SELECT AN OFFER AND CLICK OK" | Select -ExpandProperty Offer 
 $sku = Get-AzureRmVMImageSKU -Location $location -PublisherName $publisher -Offer $offer |
-    Out-GridView -PassThru -Title "SELECT AN SKU AND CLICK OK" | Select -ExpandProperty SKUs
-Get-AzureRmVMImage -Location $location -PublisherName $publisher -Offer $offer -Skus $sku | Out-Gridview -PassThru -Title "SELECT AN IMAGE and CLICK OK"
+    Out-GridView -OutputMode Single -Title "SELECT AN SKU AND CLICK OK" | Select -ExpandProperty SKUs
+Get-AzureRmVMImage -Location $location -PublisherName $publisher -Offer $offer -Skus $sku | Out-Gridview -OutputMode Single -Title "SELECT AN IMAGE and CLICK OK"
