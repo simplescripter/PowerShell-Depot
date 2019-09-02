64f5fcb19839cf8708df4a18a57d26df6a330539
simplescripter <simplescripter@gmail.com>
2019-09-01 19:54:54 -0600

    Added AudibleNotification.ps1

---
+++ AudibleNotification.ps1
@@ -0,0 +1,18 @@
+﻿$obj = New-Object System.Media.SoundPlayer
+$obj.SoundLocation = "C:\Users\Shawn.ELYSIUM\Documents\Classes\Scripting Reference\PowerShell\psinvaders\samples\insertcoin.wav"
+$ok = $false
+Do{
+    Sleep 5
+    If(Test-NetConnection 8.8.8.8 -InformationLevel Quiet){
+        $obj.Play()
+        Sleep 1
+        $obj.Play()
+        Sleep 1
+        $obj.Play()
+        Sleep 1
+        $obj.Play()
+        Sleep 1
+        $obj.Play()
+        $ok = $true
+    }
+}Until($ok -eq $true)
