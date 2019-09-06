d36daaf26608b28d66cc2fd4210374e8c5012035
Shawn Stugart <simplescripter@gmail.com>
2019-09-06 10:20:26 -0600

    colors

---
+++ ConsoleColors.ps1
@@ -0,0 +1,9 @@
+﻿For($i = 0;$i -le 15;$i++){
+    For($j = 0;$j -le 15;$j++){
+        $background = '{0:X}' -f $i
+        $foreground = '{0:X}' -f $j
+        cmd /c color $background$foreground
+        Write-Host "Color scheme # $background$foreground"
+        Sleep 1
+    }
+}
