21189c1daca71732642475e1382f97791915ce7e
Shawn Stugart <simplescripter@gmail.com>
2019-10-01 15:11:37 -0600

    new scripts

---
+++ ComparePPTHashes.ps1
@@ -0,0 +1,24 @@
+﻿Param(
+    $firstFolder,
+    $secondFolder
+)
+
+$files = Get-ChildItem $firstFolder
+#$files2 = Get-ChildItem $secondFolder | Select -ExpandProperty FullName
+ForEach($file in $files){
+    $File1 = Get-FileHash $file.FullName -Algorithm SHA1 | Select -ExpandProperty Hash
+    $File2 = Get-FileHash (Join-Path $secondFolder $file.Name) -Algorithm SHA1 | Select -ExpandProperty Hash
+    If($File1 -eq $File2){
+        $result = 'SAME'
+    }Else{
+        $result = 'CHANGED'
+    }
+    $props = [ordered]@{
+        'Name' = $file.Name
+        'File1' = $file1
+        'File2' = $file2
+        'Comparison' = $result
+    }
+    New-Object -TypeName psobject -Property $props
+}
+
---
+++ FinallyDemo.ps1
@@ -0,0 +1,33 @@
+﻿Try{
+    Get-Service NONE -ErrorAction Stop
+}
+Catch{
+
+}
+"This code runs"
+
+Try{
+    Get-Service NONE -ErrorAction Stop
+}
+Catch{
+
+}Finally{
+    "This code runs"
+}
+
+Try{
+    Get-Service NONE -ErrorAction Stop
+}
+Catch{
+    Return # or Exit
+}
+"This code does NOT run"
+
+Try{
+    Get-Service NONE -ErrorAction Stop
+}
+Catch{
+    Return #or Exit
+}Finally{
+    "This code runs"
+}
