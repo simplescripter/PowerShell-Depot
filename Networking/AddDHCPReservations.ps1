fab274ad5ecf093644446c87fa33e9ad3b7d35ea
Shawn Stugart <simplescripter@gmail.com>
2020-01-31 10:37:39 -0800

    Update AddDHCPReservations.ps1

--- Networking/AddDHCPReservations.ps1
+++ Networking/AddDHCPReservations.ps1
@@ -1,7 +1,16 @@
-﻿Function Search-Subnet {
+Param(
+    [string]$subnet = '172.16.0.',
+    [array]$hosts = 9..11,
+    [string]$fakeMACPrefixNoPing = 'AA-AA-AA',
+    [string]$fakeMACPrefixNoWMI = 'BB-BB-BB',
+    [string]$DHCPServer,
+    [string]$DHCPScopeId
+)
+
+Function Search-Subnet {
     Param(
-        [string]$subnet = '172.16.0.',
-        [array]$hosts = 8..11
+        [string]$subnet,
+        [array]$hosts
     )
     ForEach($hostIP in $hosts){
         # NOTE: because we're connecting to WMI using an IP, it's a good idea
@@ -10,26 +19,40 @@
         $computer = "$subnet$hostIP"
         $pingResult = Test-NetConnection -ComputerName $computer -WarningAction SilentlyContinue
         If($pingResult.PingSucceeded){
-            # Add error handling
-            # Get-WMIObject query assumes a single physical NIC
-            $nic = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $computer -Filter "IPEnabled=$true"
             $properties = @{
                 PingSucceeded = $pingResult.PingSucceeded
                 IPAddress = $pingResult.RemoteAddress
-                hostName = $nic.PSComputerName
-                ClientId = $($nic.MACAddress -replace ':','-')
             }
-        }<#Else{
+           
+            Try{
+                # Get-WMIObject query assumes a single physical NIC
+                $nic = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $computer -Filter "IPEnabled=$true" -ErrorAction Stop
+                $properties.Add('hostName',$nic.PSComputerName)
+                $properties.Add('ClientId',$($nic.MACAddress -replace ':','-'))
+            }Catch{
+                $properties.Add('hostname','WMI_FAILED')
+                $properties.Add('ClientId',(New-RandomMACSuffix -MACPrefix $fakeMACPrefixNoWMI))
+            }
+        }Else{
             $properties = @{
                 PingSucceeded = $pingResult.PingSucceeded
                 IPAddress = $pingResult.RemoteAddress
                 hostName = "UNAVAILABLE"
-                ClientId = "UNAVAILABLE"
+                ClientId = (New-RandomMACSuffix -MACPrefix $fakeMACPrefixNoPing)
             }
-        }#>
+        }
         $obj = New-Object -TypeName PSObject -Property $properties
         Write-Output $obj
     }
 }
 
-Search-Subnet | Add-DhcpServerv4Reservation -ComputerName 'LON-DC1' -ScopeId '172.16.0.0'
+Function New-RandomMACSuffix {
+    Param(
+        [string]$MACPrefix
+    )
+    $MACSuffix = '{0:X6}' -f (Get-Random -Maximum 16777215)
+    $MACSuffix = $MACSuffix -replace '(\w{2})(\w{2})(\w{2})', '$1-$2-$3'
+    Write-Output "$MACPrefix-$MACSuffix"
+}
+
+Search-Subnet -subnet $subnet -hosts $hosts | Add-DhcpServerv4Reservation -ComputerName $DHCPServer -ScopeId $DHCPScopeId -ErrorAction SilentlyContinue
