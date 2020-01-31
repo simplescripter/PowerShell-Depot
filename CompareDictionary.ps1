f0e686d1bd548cd3c58803182d32b0cf5c985cac
simplescripter <simplescripter@gmail.com>
2020-01-31 09:00:05 -0500

    updates

---
+++ CompareDictionary.ps1
@@ -0,0 +1,77 @@
+﻿Param(
+    [switch]$includeEqual
+)
+$table1 = @{
+    ServerA = '1.1.1.1'
+    ServerB = '1.1.1.2'
+    ServerD = '1.1.1.4'
+}
+
+$table2 = @{
+    ServerA = '1.1.1.1'
+    ServerB = '1.1.1.23'
+    ServerC = '1.1.1.3'
+}
+
+$results = @()
+# Initialize an Arraylist to hold the $table2 entries:
+$unmatchedTable2Keys = [System.Collections.ArrayList]::new()
+ForEach ($table2Entry in $table2.GetEnumerator().Name){
+    $unmatchedTable2Keys.Add($table2Entry) | Out-Null
+}
+
+ForEach($entry in $table1.GetEnumerator()){
+    $propList = [ordered]@{
+        Table_1_Key = $entry.Name
+        Table_1_Value = $entry.Value
+    }
+    # Does the key in the first table exist in the second table?
+
+    If($entry.Name -in $table2.Keys){
+        
+        # If it does, remove it from $unmatchedTable2Items:
+        $unmatchedTable2Keys.Remove($entry.Name)
+
+        # Is the value the same or different between the two tables?
+
+        If($entry.Value -eq $table2.($entry.Name)){
+            If($includeEqual){
+                $propList.Add('Equality','==')
+                $propList.Add('Table_2_Key',"$($entry.Name)")
+                $propList.Add('Table_2_Value',"$($table2.($entry.Key))")
+                $results += New-Object -TypeName PSObject -Property $propList
+            }
+        }Else{
+            # mismatch - record the difference
+            $propList.Add('Equality','<>')
+            $propList.Add('Table_2_Key',"$($entry.Name)")
+            $propList.Add('Table_2_Value',"$($table2.($entry.Key))")
+            $results += New-Object -TypeName PSObject -Property $propList
+            #"$($entry.Name) $($entry.Value) <> $($entry.Name) $($table2.($entry.Key))" 
+        }
+    }Else{
+        # mismatch - There's a new entry in the first table
+        $propList.Add('Equality','<=')
+        $propList.Add('Table_2_Key','-')
+        $propList.Add('Table_2_Value','-')
+        $results += New-Object -TypeName PSObject -Property $propList
+        #"$($entry.Name) = $($entry.Value) <= NEW"
+    }
+}
+
+# Check if there are any entries remaining in the second table:
+If($unmatchedTable2Keys.Count -gt 0){
+    ForEach($key in $unmatchedTable2Keys){
+        $propList = [ordered]@{
+            Table_1_Key = '-'
+            Table_1_Value = '-'
+            Equality = '=>'
+            Table_2_Key = $key
+            Table_2_Value = $table2.$key
+        }
+        $results += New-Object -TypeName PSObject -Property $propList
+        #"NEW => $key = $($table2.$key)"
+    }
+} 
+
+$results
--- Networking/AddDHCPReservations.ps1
+++ Networking/AddDHCPReservations.ps1
@@ -1,30 +1,33 @@
-﻿$subnet = '172.16.0.'
-$hosts = 9..11
-$dhcpServer = 'LON-DC1'
-ForEach($hostIP in $hosts){
-    # NOTE: because we're connecting to WMI using an IP, it's a good idea
-    # to make sure you've DNS reverse lookup records for the machine.  Otherwise,
-    # Kerberos auth will take MUCH longer for each IP
-    $computer = "$subnet$hostIP"
-    $pingResult = Test-NetConnection -ComputerName $computer
-    If($pingResult.PingSucceeded){
-        # Add error handling
-        # Get-WMIObject query assumes a single physical NIC
-        $nic = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $computer -Filter "IPEnabled=$true"
-        $properties = @{
-            PingSucceeded = $pingResult.PingSucceeded
-            IP = $pingResult.RemoteAddress
-            Name = $nic.__SERVER
-            MACaddress = $nic.MACAddress
-        }
-    }Else{
-        $properties = @{
-            PingSucceeded = $pingResult.PingSucceeded
-            IP = $pingResult.RemoteAddress
-            Name = "UNAVAILABLE"
-            MACaddress = "UNAVAILABLE"
-        }
-    }
-    $obj = New-Object -TypeName PSObject -Property $properties
-    Write-Output $obj
+﻿Function Search-Subnet {
+    $subnet = '172.16.0.'
+    $hosts = 9..11
+    $dhcpServer = 'LON-DC1'
+    ForEach($hostIP in $hosts){
+        # NOTE: because we're connecting to WMI using an IP, it's a good idea
+        # to make sure you've DNS reverse lookup records for the machine.  Otherwise,
+        # Kerberos auth will take MUCH longer for each IP
+        $computer = "$subnet$hostIP"
+        $pingResult = Test-NetConnection -ComputerName $computer
+        If($pingResult.PingSucceeded){
+            # Add error handling
+            # Get-WMIObject query assumes a single physical NIC
+            $nic = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $computer -Filter "IPEnabled=$true"
+            $properties = @{
+                PingSucceeded = $pingResult.PingSucceeded
+                IP = $pingResult.RemoteAddress
+                Name = $nic.__SERVER
+                MACaddress = $nic.MACAddress
+            }
+        }<#Else{
+            $properties = @{
+                PingSucceeded = $pingResult.PingSucceeded
+                IP = $pingResult.RemoteAddress
+                Name = "UNAVAILABLE"
+                MACaddress = "UNAVAILABLE"
+            }
+        }#>
+        $obj = New-Object -TypeName PSObject -Property $properties
+        Write-Output $obj
 }
+
+Search-Subnet | Add-DhcpServerv4Reservation -ComputerName 'LON-DC1' -ScopeId '172.16.0.0'
