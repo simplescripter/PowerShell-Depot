8642eaeb10db5365e4d16ffa9ce0b8087b235817
Shawn Stugart <simplescripter@gmail.com>
2020-07-03 19:03:12 -0600

    fixed logic error

--- StartContinousPing.ps1
+++ StartContinousPing.ps1
@@ -1,19 +1,19 @@
 Function Start-ContinuousPing {
     Param(
-        $nameOrIP,
+        [string]$nameOrIP,
         [int]$interval = 5
     )
     $key = $null
     $global:ProgressPreference = 'SilentlyContinue'
     
-    If($PSBoundParameters -contains 'nameOrIP'){
+    If($PSBoundParameters.Keys -contains 'nameOrIP'){
         Write-Host "Pinging $nameOrIP every $interval seconds. Hit any key to quit." -BackgroundColor White -ForegroundColor DarkMagenta
     }else{
         Write-Host "Pinging internetbeacon.msedge.net every $interval seconds. Hit any key to quit." -BackgroundColor White -ForegroundColor DarkMagenta
     }
     Do{
         $timeStamp = (Get-Date).ToLongTimeString()
-        If($PSBoundParameters -contains 'nameOrIP'){
+        If($PSBoundParameters.Keys -contains 'nameOrIP'){
             $result = Test-NetConnection $nameOrIP -WarningAction SilentlyContinue
         }else{
             $result = Test-NetConnection -WarningAction SilentlyContinue
@@ -27,3 +27,5 @@ Function Start-ContinuousPing {
     Write-Host "Key pressed. Quitting" -ForegroundColor DarkRed -BackgroundColor White
     $global:ProgressPreference = 'Continue'
 }
+
+Start-ContinuousPing -nameOrIP 8.8.8.8 -interval 3
