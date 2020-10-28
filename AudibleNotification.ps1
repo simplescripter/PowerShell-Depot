948b57aa55896054ebc4478ff10a03529569c26b
Shawn Stugart <simplescripter@gmail.com>
2020-10-28 11:24:59 -0600

    create RemoveArchiveAttribute

--- AudibleNotification.ps1
+++ AudibleNotification.ps1
@@ -2,8 +2,20 @@
 $obj.SoundLocation = "D:\Users\Shawn.ELYSIUM\Documents\Scripting Reference\PowerShell\psinvaders\samples\insertcoin.wav"
 $ok = $false
 Do{
-    Sleep 5
+    Sleep 20
     If(Test-NetConnection 8.8.8.8 -InformationLevel Quiet){
+        $obj.Play()
+        Sleep 1
+        $obj.Play()
+        Sleep 1
+        $obj.Play()
+        Sleep 1
+        $obj.Play()
+        Sleep 1
+        $obj.Play()
+        Sleep 1
+        $obj.Play()
+        Sleep 1
         $obj.Play()
         Sleep 1
         $obj.Play()
